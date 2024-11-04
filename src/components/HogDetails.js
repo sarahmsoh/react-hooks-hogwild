1f518e2f01b5666ba4ba1d396e0e03ddfdb090dd
sarahmsoh <sarahmuriithi523@gmail.com>
2024-11-04 13:13:11 +0300

    added

---
+++ src/components/HogDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HogDetails({ hog }) {
+  const { greased, weight, "highest medal achieved": medal } = hog
+  return (
+    <div className="description">
+      <strong>{greased ? 'Greased' : 'Nongreased'}</strong>
+      <p>
+        Highest medal achived: <strong>{medal}</strong>
+      </p>
+      <p>
+        Weight : <strong>{weight}</strong>
+      </p>
+    </div>
+  )
+}
+
+export default HogDetails
